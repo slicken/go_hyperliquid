ccacd57156afd60784d6ab063c8784d6b82fabbb
slicken <slicken@gmail.com>
2025-08-09 09:31:20 +0200

    v0.0.5

--- examples/example2.go
+++ examples/example2.go
@@ -34,95 +34,97 @@ func main() {
 		log.Println("WebSocket connected successfully")
 	}
 
-	meta, err := hl.GetMeta()
-	if err != nil {
-		log.Fatalf("Error getting meta: %v", err)
-	} else {
-		for _, asset := range meta.Universe {
-			err = hl.WebSocketAPI.SubscribeTrades(asset.Name, func(data interface{}) {
-				trades, ok := data.([]interface{})
-				if !ok {
-					log.Fatalf("error: %v", ok)
-				}
-				log.Printf("%s: %d trades", asset.Name, len(trades))
-			})
-			if err != nil {
-				log.Fatalf("Failed to subscribe to trades: %v", err)
+	err = hl.WebSocketAPI.SubscribeUserFills(config.AccountAddress, func(data interface{}) {
+		// Try to assert the expected structure: map[string]interface{} with "isSnapshot" key
+		if m, ok := data.(map[string]interface{}); ok {
+			if isSnapshot, ok := m["isSnapshot"].(bool); ok && isSnapshot {
+				// Don't print snapshot
+				return
 			}
-			err = hl.WebSocketAPI.SubscribeOrderUpdates(config.AccountAddress, func(data interface{}) {
-				log.Printf("%s: ORDER UPDATE RECEIVED", asset.Name)
-				orders, ok := data.([]interface{})
-				if !ok {
-					log.Printf("Order data is not array: %T - %+v", data, data)
-					return
-				}
-				for _, orderData := range orders {
-					orderMap, ok := orderData.(map[string]interface{})
-					if !ok {
-						continue
-					}
-					orderInfo, ok := orderMap["order"].(map[string]interface{})
-					if !ok {
-						continue
-					}
-					orderID, ok := orderInfo["oid"].(float64)
-					if !ok {
-						continue
-					}
-					coin, ok := orderInfo["coin"].(string)
-					if !ok {
-						continue
-					}
-
-					fmt.Printf("Cancelling order with ID: %d\n", int64(orderID))
-					cancelResponse, err := hl.ExchangeAPI.CancelOrderByOID(coin, int64(orderID))
-					if err != nil {
-						log.Printf("Failed to cancel order: %v", err)
-					} else {
-						fmt.Printf("Order cancelled successfully: %+v\n", cancelResponse)
-					}
-
-				}
-			})
-			if err != nil {
-				log.Fatalf("Failed to subscribe to order updates: %v", err)
-			}
-
 		}
+		log.Printf("%+v\n-----\n", data)
+	})
+	if err != nil {
+		log.Printf("error: %v", err)
 	}
 
-	log.Println("Subscribed to all assets")
-	time.Sleep(10 * time.Second)
-	go func() {
-		for {
-			log.Println("Starting trade loop")
+	log.Println("Subscribed.")
 
-			// Create a limit order
-			orderRequest := hyperliquid.OrderRequest{
-				Coin:    "BTC",
-				IsBuy:   true,
-				Sz:      0.001,    // Small size for testing
-				LimitPx: 109000.0, // Example price
-				OrderType: hyperliquid.OrderType{
-					Limit: &hyperliquid.LimitOrderType{
-						Tif: hyperliquid.TifGtc, // Good till cancelled
+	time.Sleep(2 * time.Second)
+	i := 0
+	go func() {
+		var orderRequest hyperliquid.OrderRequest
+
+		for {
+			if i%2 == 0 {
+				i++
+				log.Printf("sending Marketorder\n\n")
+
+				orderRequest = hyperliquid.OrderRequest{
+					Coin:    "BTC",
+					IsBuy:   false,
+					Sz:      0.0002,
+					LimitPx: 109000.0,
+					OrderType: hyperliquid.OrderType{
+						Limit: &hyperliquid.LimitOrderType{
+							Tif: hyperliquid.TifIoc, // Ioc = Market order
+						},
 					},
-				},
-				ReduceOnly: false,
-				Cloid:      hyperliquid.GetRandomCloid(),
+					ReduceOnly: false,
+					Cloid:      hyperliquid.GetRandomCloid(),
+				}
+
+			} else {
+				i++
+				log.Printf("sending Limitorder\n\n")
+
+				orderRequest = hyperliquid.OrderRequest{
+					Coin:    "BTC",
+					IsBuy:   true,
+					Sz:      0.0002,
+					LimitPx: 109000.0,
+					OrderType: hyperliquid.OrderType{
+						Limit: &hyperliquid.LimitOrderType{
+							Tif: hyperliquid.TifGtc, // Gtc = Limit order
+						},
+					},
+					ReduceOnly: false,
+					Cloid:      hyperliquid.GetRandomCloid(),
+				}
 			}
 
-			fmt.Printf("Placing order: %+v\n", orderRequest)
+			// fmt.Printf("Placing order: %+v\n", orderRequest)
 			response, err := hl.ExchangeAPI.Order(orderRequest, hyperliquid.GroupingNa)
 			if err != nil {
-				log.Printf("Failed to place order: %v", err)
-			} else {
-				fmt.Printf("Order placed successfully: %+v\n", response)
+				log.Printf("FAILED: %v", err)
 			}
+			_ = response
+			// else {
+			// 	fmt.Printf("SUCCESS: %+v\n", response)
+			// }
 
 			time.Sleep(10 * time.Second)
 		}
 	}()
+
+	/* OUTPUT:
+
+	2025/08/09 09:28:59 Using account address: 0x70173d313e536bd2858f4cbaa59bbd9451dc97bd
+	2025/08/09 09:29:03 WebSocket connected successfully
+	2025/08/09 09:29:03 Subscribed.
+	2025/08/09 09:29:05 sending Marketorder // Market order
+
+	2025/08/09 09:29:06 map[fills:[map[cloid:0x328a6818fe077c09ebd7aca549b8c2a9 closedPnl:0.0 coin:BTC crossed:true dir:Open Short fee:0.009598 feeToken:USDC hash:0x63c80338e0da3fd8d24804292c9d3d02025a005d1c32b79c29f0ea515d538a84 oid:1.29575891351e+11 px:116945.0 side:A startPosition:0.0 sz:0.0002 tid:5.45600099941465e+14 time:1.754724546288e+12 twapId:<nil>]] user:0x70173d313e536bd2858f4cbaa59bbd9451dc97bd]
+	-----
+	2025/08/09 09:29:16 sending Limitorder
+
+	2025/08/09 09:29:28 sending Marketorder
+
+	2025/08/09 09:29:30 map[fills:[map[cloid:0x5171005c71224adb1859944a27117950 closedPnl:0.0 coin:BTC crossed:true dir:Open Short fee:0.009599 feeToken:USDC hash:0xdeae50346937c95e378c04292c9e74020171009c254481e94534ac864cd9f540 oid:1.29576017649e+11 px:116952.0 side:A startPosition:-0.0002 sz:0.0002 tid:4.97570931090393e+14 time:1.75472456949e+12 twapId:<nil>]] user:0x70173d313e536bd2858f4cbaa59bbd9451dc97bd]
+	-----
+	^C
+
+	*/
 
 	// Wait for interrupt signal
 	sigChan := make(chan os.Signal, 1)
